c8a7c6c1afe61090ad7e5e9e28192a1632e5ac3d
Sam Gunner <me@sgunner.me>
2024-03-26 17:38:29 +0000

    Attempt to fix tests

--- tests/investors.spec.ts
+++ tests/investors.spec.ts
@@ -14,7 +14,7 @@ test.describe("Investors", () => {
     await page.waitForLoadState("networkidle");
 
     await expect(page.locator("#__investors-title")).toBeVisible();
-    expect(await page.locator("#__investor-row").count()).toEqual(4);
+    await expect(page.locator("#__investor-row")).toHaveCount(4);
   });
 
   test("can navigate from investor list to investor page", async ({ page }) => {
@@ -29,9 +29,8 @@ test.describe("Investors", () => {
     await page.waitForLoadState("networkidle");
 
     await expect(page.locator("#__investors-title")).toBeVisible();
-    expect(await page.locator("#__investor-row").count()).toEqual(4);
+    await expect(page.locator("#__investor-row")).toHaveCount(4);
 
-    // http://localhost:8000/api/investor/commitment/pe/2670
     await page.route("**/api/investor/commitment/*/*", (route) => {
       route.fulfill({
         status: 200,
@@ -45,6 +44,6 @@ test.describe("Investors", () => {
     await page.waitForTimeout(200); // Needed for chromium to pass
 
     await expect(page.locator("#__investor-2670-title")).toBeVisible();
-    expect(await page.locator("#__commitment-row").count()).toEqual(16);
+    await expect(page.locator("#__commitment-row")).toHaveCount(16);
   });
 });
